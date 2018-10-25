cde24f5fdb2e015313b7ef6a1b5cd828a23c5083
RayenStrasser-King <rfstrasser-king@mail.csuchico.edu>
2018-10-24 17:08:07 -0700

    XandO

--- main.cpp
+++ main.cpp
@@ -49,6 +49,22 @@ int main()
     //1-C.  In any other case, then...
     //1-C-1.  turn should be assigned the value 'X'
     
+    else
+ 
+ {
+      board[row][column] = turn;
+    
+      if (turn == 'X')
+    {
+            turn = 'O';
+    }
+
+    else
+    {
+            turn = 'X';
+    }
+ }
+
     cout<<"\nBOARD\n-----\n";
     //TODO: Print the current board
     //Outline
@@ -57,7 +73,16 @@ int main()
     //1-A-1.  Display the value of the board at location of r and c
     //1-A-2. Display a space
     //1-B. Display an newline to move to the next row of the board
-    
+   
+    for (int r = 0; r < ROWS; r++) 
+    {
+            for (int c = 0; c < COLUMNS; c++)
+            {
+                cout<<board[r][c]<<" ";
+               
+            }cout<<endl;
+    }
+
   }while( playing );
 
   cout<<"Goodbye!\n";
